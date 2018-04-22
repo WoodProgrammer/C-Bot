e25827f76e96718d32352345ad293daa038aabe9
WoodProgrammer <emirozbir@hotmail.com>
2018-04-22 18:46:21 +0300

    dockerizing removed

--- bot/bot_helper.py
+++ bot/bot_helper.py
@@ -1,26 +1,15 @@
-import os
-from jinja2 import Environment, FileSystemLoader
 from bot_model import BotModel
 
 b_obj = BotModel()
-def run_payload(payload_name,vars):
 
-    data = b_obj._get_detail(payload_name=payload_name)
-    os.system("python3" +" "+payload_name+".py"+vars)
+def run_payload(path,payload_name,vars):
+    data = get_vars(payload_name=payload_name)
+    if len(vars) != data['vars']:
+        return "Err"
+    else:
+        os.system("python3"+payload_name+".py"+vars)
 
-def verify_vars(payload_name):
-    data = b_obj._get_detail(payload_name=payload_name)
-    print(data['vars'])
 
 
-def dockerize_function(payload_name):## build generated docker file
-    file = b_obj._get_detail(payload_name=payload_name)
-
-def generate_docker_file(payload_name):###generating docker file
-
-    env = Environment(loader=FileSystemLoader('templates'))
-    template = env.get_template('Dockerfile')
-    output_from_parsed_template = template.render(payload=payload_name,payload_file=payload_name+".py")
-    print(output_from_parsed_template)
-    with open(payload_name, "w") as fh:
-        fh.write(output_from_parsed_template)
+def get_vars(payload_name):
+    return b_obj._get_detail(payload_name=payload_name)
--- bot/test_bot_helper.py
+++ bot/test_bot_helper.py
@@ -1,15 +1,10 @@
-from bot_helper import run_payload, verify_vars, generate_docker_file
+from bot_helper import run_payload, get_vars
 import pytest
 
-payload_name = "docker"
+payload_name = "docker_registry"
 
-@pytest.mark.skip(reason="Stopped")
 def test_run_payload():
-    return run_payload(payload_name)
+    run_payload(payload_name)
 
-
-def test_verify_vars():
-    verify_vars('docker')
-
-def test_generate_docker_file():
-    generate_docker_file(payload_name=payload_name)
+def test_get_vars():
+    get_vars(payload_name='docker_registry')
